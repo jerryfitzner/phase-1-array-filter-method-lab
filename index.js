f091f9eba1f9e08972fffd85d7553f60a6f50cd9
Jerry Fitzner <jfitzner1@gmail.com>
2021-12-29 13:17:00 -0600

    commit tree c5a95baec8f231ad184e8496f12d7a7fff427d2f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(array, string){
+    return array.filter(function(match){
+        return match.toLowerCase() === string.toLowerCase()
+    })
+};
+
+function fuzzyMatch(array, string){
+    return array.filter(function(beg){
+        return beg.indexOf(string) === 0
+    })
+};
+
+function matchName(array, string){
+    return array.filter(function(test){
+        return test.name === string;
+    })
+
+}
